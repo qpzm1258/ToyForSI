e42d3cca044ff2690d38296ae193967b60056d5c
qpzm1258 <qpzm1258@163.com>
2017-12-13 15:55:41 +0800

    完成部门管理

--- Controllers/DepartmentController.cs
+++ Controllers/DepartmentController.cs
@@ -9,6 +9,7 @@ using ToyForSI.Data;
 using ToyForSI.Models;
 using ToyForSI.Controllers.Enum;
 
+
 namespace ToyForSI.Controllers
 {
     public class DepartmentController : Controller
@@ -44,6 +45,7 @@ namespace ToyForSI.Controllers
                 return NotFound();
             }
 
+            GetAttributes(id);
             return View(department);
         }
 
@@ -51,6 +53,7 @@ namespace ToyForSI.Controllers
         public IActionResult Create()
         {
             step=ActionStep.create;
+            GetAttributes(null);
             return View();
         }
 
@@ -59,12 +62,17 @@ namespace ToyForSI.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("departmentId,departmentName")] Department department)
+        public async Task<IActionResult> Create([Bind("departmentId,departmentName")] Department department, IDictionary<int,string> dictionary)
         {
             step=ActionStep.create;
             if (ModelState.IsValid)
             {
                 _context.Add(department);
+                //await _context.SaveChangesAsync();
+                foreach(KeyValuePair<int,string> value in dictionary)
+                {
+                    _context.Add(new DepartmentValue { departmentAttributeId = value.Key,department=department,departmentValue = value.Value });
+                }
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
@@ -85,6 +93,7 @@ namespace ToyForSI.Controllers
             {
                 return NotFound();
             }
+            GetAttributes(id);
             return View(department);
         }
 
@@ -93,7 +102,7 @@ namespace ToyForSI.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("departmentId,departmentName")] Department department)
+        public async Task<IActionResult> Edit(int id, [Bind("departmentId,departmentName")] Department department, IDictionary<int, string> dictionary)
         {
             step=ActionStep.edit;
             if (id != department.departmentId)
@@ -106,6 +115,16 @@ namespace ToyForSI.Controllers
                 try
                 {
                     _context.Update(department);
+                    var departmentValues = _context.DepartmentValue.Where(v => v.departmentId == id);
+                    foreach (KeyValuePair<int,string> value in dictionary)
+                    {
+                        DepartmentValue dv=departmentValues.SingleOrDefault(v => v.departmentValueId == value.Key);
+                        if (dv != null)
+                        {
+                            dv.departmentValue = value.Value;
+                            _context.Update(dv);
+                         }
+                    }
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
@@ -180,6 +199,68 @@ namespace ToyForSI.Controllers
             }
 
             return Json(data: true);
+        }
+
+        private void GetAttributes(int? departmentid)
+        {
+            // var departmentvalue = from a in _context.DepartmentAttributes
+            //                       orderby a.departmentAttributeId
+            //                       join v in _context.DepartmentValue on a.departmentAttributeId equals v.departmentAttributeId
+            //                       //where view.departmentId == departmentid
+            //                       select new
+            //                       {
+            //                           departmentId = v.departmentId,
+            //                           departmentAttributeId = v.departmentAttributeId,
+            //                           departmentValueId = v.departmentValueId,
+            //                           departmentAttributeName = a.departmentAttributeName,
+            //                           departmentValue = v.departmentValue
+            //                       } into g
+            //                       where g.departmentId == departmentid
+            //                       select g;
+            if (departmentid.HasValue)
+            {
+                SyncValueTable(departmentid);
+
+                var departmentvalue = from a in _context.DepartmentAttributes
+                                      orderby a.departmentAttributeId
+                                      join v in _context.DepartmentValue on a.departmentAttributeId equals v.departmentAttributeId into g
+                                      from r in g.DefaultIfEmpty()
+                                      where r.departmentId == departmentid
+                                      select new DepartmentAttributeAndValueView
+                                      {
+                                          aId = a.departmentAttributeId,
+                                          dId = r.departmentId,
+                                          aName = a.departmentAttributeName,
+                                          vId = r.departmentValueId,
+                                          value = r.departmentValue
+                                      };
+                ViewBag.DepartmentValue = departmentvalue.ToList();
+            }
+            else
+            {
+                var attribute = _context.DepartmentAttributes;
+                List<DepartmentAttributeAndValueView> departmentvalue= new List<DepartmentAttributeAndValueView>();
+                foreach(var attr in attribute)
+                {
+                    departmentvalue.Add(new DepartmentAttributeAndValueView { aId = attr.departmentAttributeId, aName = attr.departmentAttributeName });
+                }
+                ViewBag.DepartmentValue = departmentvalue;
+            }
+
+        }
+
+        public int SyncValueTable(int? departmentid)
+        {
+            var novalue = _context.DepartmentAttributes.Where(n =>! _context.DepartmentValue.Where(m=>m.departmentId==departmentid).Select(m => m.departmentAttributeId).Contains(n.departmentAttributeId));
+            foreach(var value in novalue.ToList())
+            {
+                DepartmentValue v = new DepartmentValue();
+                v.departmentId = (int)departmentid;
+                v.departmentAttributeId = value.departmentAttributeId;
+                _context.Add(v);
+            }
+            _context.SaveChanges();
+            return 0;
         }
     }
 }
---
+++ Models/DepartmentAttributeAndValueView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToyForSI.Models
+{
+    public class DepartmentAttributeAndValueView
+    {
+        public int aId { get; set; }
+        public int? dId { get; set; }
+        public int? vId { get; set; }
+        public string aName { get; set; }
+        public string value { get; set; }
+    }
+}
